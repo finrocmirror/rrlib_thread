2762114736661d589c433b511fcf11daaf9326e5
Tobias Föhst <foehst@informatik.uni-kl.de>
2013-05-07 02:26:22 +0200

    Updated file info

--- tConditionVariable.h
+++ tConditionVariable.h
@@ -19,7 +19,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
 //----------------------------------------------------------------------
-/*!\file    rrlib/finroc_core_utils/tConditionVariable.h
+/*!\file    rrlib/thread/tConditionVariable.h
  *
  * \author  Max Reichardt
  *
